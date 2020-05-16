802ec451fe07412aa2a7e51c59beeeed59a5948c
EricCJoyce <50152689+EricCJoyce@users.noreply.github.com>
2020-05-16 02:30:12 -0400

    Create neuron.cpp

---
+++ neuron.cpp
@@ -0,0 +1,14 @@
+#ifndef __NEURON_CPP
+#define __NEURON_CPP
+
+#include "neuron.h"
+
+/**************************************************************************************************
+ Constructors  */
+
+/* PLACE-HOLDER */
+NeuralNet::NeuralNet()
+  {
+  }
+
+#endif
